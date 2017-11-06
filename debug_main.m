5ed4d90ec81b00e4b8da1100f812f62e24e4d471
EconData <32192334+DanyangSu@users.noreply.github.com>
2017-11-06 16:58:14 -0500

    Add files via upload

---
+++ debug_main.m
@@ -0,0 +1,9 @@
+a = 100;
+iter = 1;
+distance = 1;
+
+while distance>0.01
+    a1 = diverge;
+    distance = abs(a1-a);
+    a = a1;
+end
---
+++ diverge.m
@@ -0,0 +1,4 @@
+function a = diverge(b)
+    x = b + 1;
+    a = x;
+end
